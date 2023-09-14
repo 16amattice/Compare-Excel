adac0ac95c80b9baeb58c7e5101dfdd050d3e0a5
Anthony Mattice <AMattice@desert-aire.com>
2023-09-14 11:59:14 -0500

    Updates

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ const url = require("url");
 const path = require("path");
 
 import { app, BrowserWindow, dialog } from "electron";
+const log = require('electron-log');
 const { autoUpdater } = require('electron-updater');
 
+// Logging
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+
 let window: BrowserWindow | null;
+
+
 
 const createWindow = () => {
   window = new BrowserWindow({ width: 800, height: 600 });
@@ -21,44 +29,37 @@ const createWindow = () => {
     window = null;
   });
 };
+function sendStatusToWindow(text: string) {
+  log.info(text);
+  window.webContents.send('message', text);
+}
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', () => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', () => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err: string) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj: { bytesPerSecond: string; percent: string; transferred: string; total: string; }) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', () => {
+  sendStatusToWindow('Update downloaded');
+});
 
 app.on("ready", () => {
   createWindow();
   autoUpdater.checkForUpdatesAndNotify();
 });
 
-autoUpdater.on("update-available", (info: { version: any; }) => {
-  dialog.showMessageBox({
-    type: "info",
-    title: "Update available",
-    message: `A new version (${info.version}) is available. Do you want to update now?`,
-    buttons: ["Yes", "No"]
-  }).then(response => {
-    if (response.response === 0) { // If the user clicks "Yes"
-      autoUpdater.downloadUpdate();
-    }
-  });
-});
-
-autoUpdater.on("update-downloaded", () => {
-  dialog.showMessageBox({
-    type: "info",
-    title: "Update ready",
-    message: "The update is ready to be installed. The application will now close and the update will be installed.",
-    buttons: ["OK"]
-  }).then(() => {
-    autoUpdater.quitAndInstall();
-  });
-});
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
-
-app.on("activate", () => {
-  if (window === null) {
-    createWindow();
-  }
+app.on('window-all-closed', () => {
+  app.quit();
 });
